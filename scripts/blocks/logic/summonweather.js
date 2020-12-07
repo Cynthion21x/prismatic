0e681626464c02c52c413baf06d58bbd68ba6d1a
ice100k <61597736+ice100k@users.noreply.github.com>
2020-12-07 06:43:29 +0000

    Update summonweather.js

--- scripts/blocks/logic/summonweather.js
+++ scripts/blocks/logic/summonweather.js
@@ -1,80 +1,148 @@
-const edges = [[1, 0], [0, 1], [-1, 0], [0, -1]];
+const Integer = java.lang.Integer;
+const Float = java.lang.Float;
 
-var itemCache = {};
+const weatherSpawner = extendContent(Block, "summonweather", {
+});
 
-const smartsource = extendContent(Block, "smartsource", {
-  /*setBars(){
-    this.super$setBars();
-    this.bars.remove("items");
-  },*/
-  outputsItems(){
-    return true;
+weatherSpawner.config(Integer, (build, value) => {
+  if(value >= 0){
+    var weather = Vars.content.getByID(ContentType.weather, value - 1);
+    if(build.enableLayering() || !weather.isActive()) weather.create(build.wIntensity(), build.wDuration());
+  }
+  else if(value == -1) build.clearWeather();
+  else if(value == -2) build.setLayering(true);
+  else if(value == -3) build.setLayering(false);
+  else if(value == -4) build.addDuration(-60 * 30);
+  else if(value == -5) build.addDuration(60 * 30);
+});
+
+weatherSpawner.config(Float, (build, value) => {
+  if(value > 0){
+    build.setIntensity(value);
   }
 });
 
-smartsource.buildType = () => {
+weatherSpawner.buildType = () => {
   return extend(Building, {
-    _proxItems: [],
-    updateTile(){
-      if(this._proxItems.length > 0){
-        for(var i=0; i<this._proxItems.length; i++){
-          this.items.set(this._proxItems[i], 1);
-          if(Version.type == "bleeding-edge" && Version.build >= 10302) this.dump(this._proxItems[i]);//THX Anuke!
-          else this.offload(this._proxItems[i]);
+    _duration: Time.toMinutes,
+    _intensity: 1,
+    _layer: false,
+    createWeatherButton(p, weather){
+      p.button(weather.localizedName, Styles.clearPartialt, () => {
+        this.configure(new Integer(weather.id + 1));
+      }).width(160).height(40);
+      p.row();
+    },
+    buildConfiguration(table){
+      table.pane(Styles.smallPane, cons(p => {
+        const weathers = Vars.content.getBy(ContentType.weather).toArray();
+        for(var i=0; i<weathers.length; i++){
+          this.createWeatherButton(p, weathers[i]);
         }
-        this.items.clear();
+      })).height(160).width(160).top();
+      table.row();
+      table.table(Styles.black6, cons(t => {
+        t.add().pad(5);
+        t.row();
+
+        t.add("$dialog.weather-duration").height(30);
+        t.row();
+        t.table(cons(td => {
+          td.button("-", Styles.transt, () => {
+            if(this._duration/Time.toMinutes > 0.6) this.configure(new Integer(-4));
+          }).size(30);
+          td.label(prov(() => this.formatMins(this._duration) + " [lightgray]"+Core.bundle.get("unit.minutes")+"[]")).width(90).height(30).padLeft(5).padRight(5);
+          td.button("+", Styles.transt, () => {
+            if(this._duration/Time.toMinutes < 14.9) this.configure(new Integer(-5));
+          }).size(30);
+        }))
+        t.row();
+        t.add().pad(5);
+        t.row();
+
+        t.add("$dialog.weather-intensity").height(30);
+        t.row();
+        t.table(cons(td => {
+          td.label(prov(() => this._intensity + "")).width(110).height(30).padRight(10).padLeft(10);
+          td.button(Icon.pencilSmall, Styles.emptyi, () => {
+            //false inumeric to allow .
+            Vars.ui.showTextInput("", "$dialog.short.set-intensity", 10, this._intensity + "", false, cons(t => {
+              if(!isNaN(t * 1) && t >= 0){
+                this.configure(new Float(Number(t)));
+              }else{
+                Vars.ui.showErrorMessage("$dialog.short.invalid-number");
+              }
+            }));
+          }).size(30);
+        }))
+        t.row();
+        t.add().pad(5);
+        t.row();
+
+        t.check("$dialog.weather-layer", 25, this._layer, c => {
+          if(c != this._layer){
+            if(c) this.configure(new Integer(-2));
+            else this.configure(new Integer(-3));
+          }
+        }).width(160).height(30);
+        t.row();
+        t.add().pad(5);
+      }));
+      table.row();
+
+      table.button("$dialog.weather-clear", Styles.clearPartialt, () => {
+        this.configure(new Integer(-1));
+      }).width(160).height(40);
+    },
+    clearWeather(){
+      const weathers = Vars.content.getBy(ContentType.weather).toArray();
+      for(var i=0; i<weathers.length; i++){
+        while(weathers[i].isActive()){
+          weathers[i].remove();
+        }
       }
     },
-    onProximityUpdate(){
-      this.super$onProximityUpdate();
 
-      //print("Prox Start:");
-      this._proxItems = [];
-      for(var i=0; i<4; i++){
-        var otherBlock = Vars.world.build(this.tile.x + edges[i][0], this.tile.y + edges[i][1]);
-        if(otherBlock == null || otherBlock.block.update == false || !otherBlock.block.consumes.has(ConsumeType.item) || (otherBlock instanceof Turret.TurretBuild)) continue;
+    formatMins(ticks){
+      var mins = ticks/Time.toMinutes;
+      if(mins - Mathf.floorPositive(mins) < 0.1) return Mathf.floorPositive(mins);
+      else return Mathf.floorPositive(mins*10)/10;
+    },
 
-        if(itemCache[otherBlock.block.name] != undefined){
-          for(var j=0; j<itemCache[otherBlock.block.name].length; j++){
-            /*if(this._proxItems.indexOf(itemCache[otherBlock.block.name][j]) < 0)*/ this._proxItems.push(itemCache[otherBlock.block.name][j]);
-          }
-          continue;
-        }
+    enableLayering(){
+      return this._layer;
+    },
+    setLayering(a){
+      this._layer = a;
+    },
+    wIntensity(){
+      return this._intensity;
+    },
+    setIntensity(a){
+      this._intensity = Mathf.floorPositive(a*100)/100;
+    },
+    wDuration(){
+      return this._duration;
+    },
+    setDuration(a){
+      this._duration = a;
+    },
+    addDuration(a){
+      this._duration += a;
+      if(this._duration < 0) this._duration = 0;
+    },
 
-        itemCache[otherBlock.block.name] = [];
-        try{
-          var otherCons = otherBlock.block.consumes.getItem();
-          for(var k=0; k<otherCons.items.length; k++){
-            var itemCons = otherCons.items[k].item;
-            /*if(this._proxItems.indexOf(itemCons) < 0)*/ this._proxItems.push(itemCons);
-            itemCache[otherBlock.block.name].push(itemCons);
-          }
-        }
-        catch(toBeFixedLater){}
-      }
-      //print("ProxItems: "+this._proxItems);
-
-      //print("h");
-    }
-  });
-}
-
-
-const smartliquid = extendContent(Block, "smartliquid", {
-  init(){
-    this.super$init();
-    this.liqArr = Vars.content.liquids().toArray();
-  }
-});
-
-smartliquid.buildType = () => {
-  return extend(Building, {
-    updateTile(){
-      //just cycle through all liquids because there are much less liquids than items(if not, you are doing something horribly wrong).
-      for(var i=0; i<smartliquid.liqArr.length; i++){
-        this.liquids.add(smartliquid.liqArr[i], smartliquid.liquidCapacity);
-        this.dumpLiquid(smartliquid.liqArr[i]);
-      }
-    }
+    read(stream, version){
+      this.super$read(stream, version);
+      this._layer = stream.bool();
+      this._duration = stream.s();
+      this._intensity = stream.f();
+    },
+    write(stream){
+      this.super$write(stream);
+      stream.bool(this._layer);
+      stream.s(this._duration);
+      stream.f(this._intensity);
+    },
   });
 }
