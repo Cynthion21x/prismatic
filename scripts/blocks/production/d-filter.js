67a0c5fddfd3628bf1b0b40024d6b6c912f6a599
ice100k <61597736+ice100k@users.noreply.github.com>
2020-12-15 18:48:20 +0000

    bye script

--- scripts/blocks/production/d-filter.js
+++ scripts/blocks/production/d-filter.js
@@ -1,16 +1,1 @@
-const dfilter = extendContent(GenericCrafter, "d-filter", {
-	load() {
-		this.super$load();
-		this.regions = [];
-		this.regions[0] = Core.atlas.find(this.name);
-		this.regions[1] = Core.atlas.find(this.name + "-liquid");
-	}
-});
 
-dfilter.buildType = () => extendContent(GenericCrafter.GenericCrafterBuild,dfilter, {
-	draw() {
-	    Draw.rect(dfilter.regions[0], this.x, this.y);
-	    Drawf.liquid(dfilter.regions[1], this.x, this.y, this.liquids.get(dfilter.outputLiquid.liquid) / dfilter.liquidCapacity, Liquids.d-wave.color);
-	    Draw.rect(dfilter.regions[2], this.x, this.y);
-	}
-});
