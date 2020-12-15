9675c874e287544fa68002cbbbc74c4043bf6dc5
ice100k <61597736+ice100k@users.noreply.github.com>
2020-12-15 18:45:02 +0000

    script test no.1

--- scripts/main.js
+++ scripts/main.js
@@ -2,5 +2,6 @@
 require("blocks/logic/overpriced-processor");
 require("blocks/logic/summonweather");
 require("blocks/defence/force-block");
+require("blocks/production/d-filter");
 
 print("Load Complete!");
