1d049e9fccbac70b599ae79ff6858659632d5335
ice100k <61597736+ice100k@users.noreply.github.com>
2020-12-21 16:05:10 +0000

    slight change

--- scripts/units/siegeengine.js
+++ scripts/units/siegeengine.js
@@ -1,6 +1,6 @@
-const siegeengine = extendContent(UnitType, "siege-engine", {});
-siegeengine.constructor = () => extend(UnitWaterMove, {});
-siegeengine.defaultController = ais.groundRepairAI;
-siegeengine.abilities.add(new UnitSpawnAbility(UnitTypes.crawler, 60 * 30, 0, -7.5));
-siegeengine.abilities.add(new StatusFieldAbility(StatusEffects.overclock, 60 * 6, 60 * 9, 64));
-siegeengine.ammoType = AmmoTypes.power;
+const Siegeengine = extendContent(UnitType, "siege-engine", {});
+Siegeengine.constructor = () => extend(UnitWaterMove, {});
+Siegeengine.defaultController = ais.groundRepairAI;
+Siegeengine.abilities.add(new UnitSpawnAbility(UnitTypes.crawler, 60 * 30, 0, -7.5));
+Siegeengine.abilities.add(new StatusFieldAbility(StatusEffects.overclock, 60 * 6, 60 * 9, 64));
+Siegeengine.ammoType = AmmoTypes.power;
