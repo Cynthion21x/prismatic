e62e34d936f19ba1745d67602b5af73090129f99
ice100k <ice100k@outlook.com>
2020-12-28 11:35:38 +0000

    siegeengine and dgenerator sprites

--- scripts/units/siegeengine.js
+++ scripts/units/siegeengine.js
@@ -3,3 +3,5 @@ Siegeengine.constructor = () => extend(UnitEntity, {});
 Siegeengine.abilities.add(new UnitSpawnAbility(UnitTypes.crawler, 60 * 30, 0, -7.5));
 Siegeengine.abilities.add(new StatusFieldAbility(StatusEffects.overclock, 60 * 6, 60 * 9, 64));
 Siegeengine.ammoType = AmmoTypes.power;
+
+Blocks.groundFactory.plans.add(new UnitFactory.UnitPlan(Siegeengine, 60 * 25, ItemStack.with(Items.silicon, 1000, Items.metaglass, 600, Items.copper, 600, Items.lead, 200)));
