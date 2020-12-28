5cd348d3b1675c3aec3dc1aa03e2df4b248f2ed6
thecrisperson1 <62443519+thecrisperson1@users.noreply.github.com>
2020-12-28 18:36:00 +0000

    Update siegeengine.js

--- scripts/units/siegeengine.js
+++ scripts/units/siegeengine.js
@@ -4,4 +4,4 @@ Siegeengine.abilities.add(new UnitSpawnAbility(UnitTypes.crawler, 60 * 30, 0, -7
 Siegeengine.abilities.add(new StatusFieldAbility(StatusEffects.overclock, 60 * 6, 60 * 9, 64));
 Siegeengine.ammoType = AmmoTypes.power;
 
-Blocks.groundFactory.plans.add(new UnitFactory.UnitPlan(Siegeengine, 60 * 25, ItemStack.with(Items.silicon, 2800, Items.plastanium, 950, Items.surge-alloy, 700, )));
+Blocks.groundFactory.plans.add(new UnitFactory.UnitPlan(Siegeengine, 60 * 25, ItemStack.with(Items.silicon, 2800, Items.plastanium, 950, Items.surge-alloy, 700)));
