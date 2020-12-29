85e718ec9052285023ca9f29c75f66f4eaa18f22
ice100k <ice100k@outlook.com>
2020-12-29 10:11:36 +0000

    2 factorys and bigger overclock

--- scripts/units/siegeengine.js
+++ scripts/units/siegeengine.js
@@ -1,7 +1,8 @@
 const Siegeengine = extendContent(UnitType, "siegeengine", {});
 Siegeengine.constructor = () => extend(UnitEntity, {});
-Siegeengine.abilities.add(new UnitSpawnAbility(UnitTypes.crawler, 60 * 30, 0, -7.5));
-Siegeengine.abilities.add(new StatusFieldAbility(StatusEffects.overclock, 60 * 6, 60 * 9, 64));
+Siegeengine.abilities.add(new UnitSpawnAbility(UnitTypes.crawler, 60 * 30, -5, -7.5));
+Siegeengine.abilities.add(new UnitSpawnAbility(UnitTypes.crawler, 60 * 30, 5, -7.5));
+Siegeengine.abilities.add(new StatusFieldAbility(StatusEffects.overclock, 60 * 9, 60 * 13, 120));
 Siegeengine.ammoType = AmmoTypes.power;
 
 Blocks.groundFactory.plans.add(new UnitFactory.UnitPlan(Siegeengine, 60 * 25, ItemStack.with(Items.silicon, 3000, Items.plastanium, 950, Items.lead, 3500, Items.copper, 3000, Items.titanium, 900)));
