8fc691e920fa8f1062cc88c7be169f2264461865
ice100k <ice100k@outlook.com>
2021-01-02 11:21:42 +0000

    bye script

--- scripts/units/siege.js
+++
@@ -1,5 +0,0 @@
-const siege = extendContent(UnitType, "chaos", {});
-siege.constructor = () => extend(UnitEntity, {});
-siege.abilities.add(new UnitSpawnAbility(UnitTypes.crawler, 60 * 30, -18, -8.5));
-siege.abilities.add(new UnitSpawnAbility(UnitTypes.crawler, 60 * 30, 18, -8.5));
-siege.ammoType = AmmoTypes.power;
