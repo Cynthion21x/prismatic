b6a766baf9acdc6b0a2b67a3d7eba2a14ac4fb91
ice100k <ice100k@outlook.com>
2021-01-02 10:42:40 +0000

    remove overckock field

--- scripts/units/siege.js
+++ scripts/units/siege.js
@@ -2,5 +2,4 @@ const siege = extendContent(UnitType, "chaos", {});
 siege.constructor = () => extend(UnitEntity, {});
 siege.abilities.add(new UnitSpawnAbility(UnitTypes.crawler, 60 * 30, -18, -8.5));
 siege.abilities.add(new UnitSpawnAbility(UnitTypes.crawler, 60 * 30, 18, -8.5));
-siege.abilities.add(new StatusFieldAbility(StatusEffects.overclock, 60 * 9, 60 * 13, 120));
 siege.ammoType = AmmoTypes.power;
