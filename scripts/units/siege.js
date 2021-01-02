b150f3855bfc01e1e574eba4512cdd8e7db9eba5
ice100k <ice100k@outlook.com>
2021-01-02 10:41:56 +0000

    re add abilities

--- scripts/units/siege.js
+++ scripts/units/siege.js
@@ -1,4 +1,4 @@
-const siege = extendContent(UnitType, "aquilia", {});
+const siege = extendContent(UnitType, "chaos", {});
 siege.constructor = () => extend(UnitEntity, {});
 siege.abilities.add(new UnitSpawnAbility(UnitTypes.crawler, 60 * 30, -18, -8.5));
 siege.abilities.add(new UnitSpawnAbility(UnitTypes.crawler, 60 * 30, 18, -8.5));
