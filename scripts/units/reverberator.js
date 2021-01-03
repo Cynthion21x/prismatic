feb6c0b25ba86bc1c7ecb07adb52be416e24ecf5
ice100k <ice100k@outlook.com>
2021-01-03 16:30:32 +0000

    effects

--- scripts/units/reverberator.js
+++ scripts/units/reverberator.js
@@ -1,2 +1,2 @@
-var upgrade = new Seq([Vars.content.getByName(ContentType.unit, "prismatic-echo"), Vars.content.getByName(ContentType.unit, "prismatic-reverberator")]);
+var upgrade = new Seq([Vars.content.getByName(ContentType.unit, "Echo"), Vars.content.getByName(ContentType.unit, "Reverberator")]);
 Blocks.additiveReconstructor.upgrades.add(upgrade.toArray(UnitType));
