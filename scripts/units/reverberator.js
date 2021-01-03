b7a98014726f9a2f4a51d27b97c272fffd8c257e
ice100k <ice100k@outlook.com>
2021-01-03 16:34:59 +0000

    unit name change test

--- scripts/units/reverberator.js
+++ scripts/units/reverberator.js
@@ -1,2 +1,2 @@
-var upgrade = new Seq([Vars.content.getByName(ContentType.unit, "Echo"), Vars.content.getByName(ContentType.unit, "Reverberator")]);
+var upgrade = new Seq([Vars.content.getByName(ContentType.unit, "prismatic-echo"), Vars.content.getByName(ContentType.unit, "prismatic-reverberator")]);
 Blocks.additiveReconstructor.upgrades.add(upgrade.toArray(UnitType));
