75efdd1173e8f3b05b3aae1fa90f8304e50b28eb
ice100k <ice100k@outlook.com>
2021-01-04 08:10:20 +0000

    remove unecessary scripts

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,8 @@
 //blocks
 require("blocks/logic/overpriced-processor");
-require("blocks/logic/summonweather");
-require("blocks/defence/force-block");
 require("blocks/production/d-filter");
 
 //units
-require("units/siege");
 require("units/reverberator");
 
 print("Load Complete!");
