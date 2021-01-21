239b563d4d615f5f5ff12759805dd4b64f652b76
ice100k <ice100k@outlook.com>
2021-01-21 09:00:20 +0000

    remove broken scripts

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,4 @@
 require("blocks/logic/overpriced-processor");
 require("blocks/production/d-filter");
 
-//units
-require("units/reverberator");
-
 print("Load Complete!");
--- scripts/units/reverberator.js
+++
@@ -1,2 +0,0 @@
-var upgrade = new Seq([Vars.content.getByName(ContentType.unit, "prismatic-echo"), Vars.content.getByName(ContentType.unit, "prismatic-reverberator")]);
-Blocks.additiveReconstructor.upgrades.add(upgrade.toArray(UnitType));
